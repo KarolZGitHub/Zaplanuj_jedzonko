924e4caf32c995fe321c6061d465bce42f0d80ce
Karol <karol.zimolag@wp.pl>
2023-04-07 19:34:56 +0200

    Probably working Filter :D

--- src/main/java/pl/coderslab/web/LoginFilter.java
+++ src/main/java/pl/coderslab/web/LoginFilter.java
@@ -2,9 +2,12 @@ package pl.coderslab.web;
 
 import javax.servlet.*;
 import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(filterName = "LoginFilter")
+@WebFilter("/app/*")
 public class LoginFilter implements Filter {
     public void init(FilterConfig config) throws ServletException {
     }
@@ -14,6 +17,16 @@ public class LoginFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        HttpSession session = httpServletRequest.getSession();
+        boolean check = session != null && session.getAttribute("login") != null && session.getAttribute("login").equals("loggedIn");
+        if (check) {
+            String login = (String) session.getAttribute("login");
+            session.setAttribute(login , "loggedIn");
+        }else {
+            httpServletResponse.sendRedirect("/login");
+        }
         chain.doFilter(request, response);
     }
 }
